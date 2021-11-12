ca15c0e2e96c4643df72ab3e30d473b3d74d6630
Salomon Levi <salolevi2@gmail.com>
2021-11-11 22:34:12 -0300

    report incident termianado

--- project_django/home/urls.py
+++ project_django/home/urls.py
@@ -3,8 +3,6 @@ from home import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="main_index"),
-    path("index", views.IndexView.as_view(), name="main_index"),
     path('create-ticket', views.CreateTicketView.as_view(), name="createticket-form"),
-    path('create-ticket/nuevo', views.CreateTicket.as_view(), name="create-ticket")
-
+    path('create-ticket/nuevo', views.CreateTicketView.as_view(), name="create-ticket")
 ]
--- project_django/home/views.py
+++ project_django/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, View
 from .models import Usuario, Analist, Planta, Incident
 
@@ -16,11 +16,11 @@ class IndexView(TemplateView):
 
         return context
 
-class CreateTicketView(TemplateView):
+class CreateTicketView(View):
     template_name = 'create-ticket.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get(self, *args, **kwargs):
+        context = {}
         usuarios = Usuario.objects.all()
         analistas = Analist.objects.all()
         plantas = Planta.objects.all()
@@ -28,7 +28,27 @@ class CreateTicketView(TemplateView):
         context['analistas'] = analistas
         context['plantas'] = plantas
 
-        return context
+        return render(self.request, self.template_name, context)
 
-class CreateTicket(View):
-    def getp
+    def post(self, *args, **kwargs):
+        new_incident = Incident()
+        new_incident.desc = self.request.POST.get('desc', None)
+        new_incident.usuario = Usuario.objects.filter(id=self.request.POST.get('user', None))[0]
+        new_incident.analista = Analist.objects.filter(id = self.request.POST.get('analist', None))[0]
+        new_incident.planta = Planta.objects.filter(id = self.request.POST.get('planta', None))[0]
+        new_incident.save()
+
+        return redirect('main_index')
+
+
+# class CreateTicket(View):
+#
+#     template_name = 'create-ticket.html'
+#
+#     def post(self, *args, **kwargs):
+#         new_incident = Incident()
+#         new_incident.desc = self.request.POST.get('desc', None)
+#         new_incident.usuario = Usuario.objects.filter(id=self.request.POST.get('user', None))
+#         new_incident.analista = Analist.objects.filter(id = self.request.POST.get('analist', None))
+#         new_incident.planta = Planta.objects.filter(id = self.request.POST.get('planta', None))
+#         new_incident.save()
